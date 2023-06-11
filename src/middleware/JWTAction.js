20f530978048cc70126958200b7c9df15df2f4b1
giatha0 <thao8c89@gmail.com>
2023-06-12 01:50:05 +0700

    viet 2 middle ware checkUserLogin and chekcUserPermission

--- src/middleware/JWTAction.js
+++ src/middleware/JWTAction.js
@@ -17,18 +17,80 @@ const createJWT = (payload) => {
 
 const verifyJWT = (token) => {
     let key = process.env.JWT_SECRET;
-    let data = null;
+    let decoded = null;
 
     try {
-        data = jwt.verify(token, key);
-        console.log(data);
-
+        decoded = jwt.verify(token, key);
     } catch (error) {
         console.log(error);
     }
-    return data;
+    return decoded;
+}
+
+const checkUserJWT = (req, res, next) => {
+    let cookies = req.cookies;
+    if (cookies && cookies.jwt) {
+        let token = cookies.jwt;
+        let decoded = verifyJWT(token);
+        if (decoded) {
+            req.user = decoded;
+            next();
+        } else {
+            return res.status(401).json({
+                EC: -1,
+                EM: 'Unauthorized',
+                DT: ''
+            })
+        }
+    } else {
+        return res.status(401).json({
+            EC: -1,
+            EM: 'Unauthorized',
+            DT: ''
+        })
+
+    }
+    // console.log(cookies);
+}
+
+const checkUserPermission = (req, res, next) => {
+    if (req.user) {
+        let email = req.user.email;
+        let roles = req.user.groupWithRoles.Roles;
+        let currentUrl = req.path;
+        console.log(currentUrl);
+        if (!roles || roles.length === 0) {
+            return res.status(403).json({
+                EC: -1,
+                EM: `You don't have permission to access this page`,
+                DT: ''
+            })
+        }
+
+        console.log(roles);
+        let canAccess = roles.some(item => item.url === currentUrl)
+
+        console.log(canAccess);
+        if (canAccess) {
+            next();
+        } else {
+            return res.status(403).json({
+                EC: -1,
+                EM: `You don't have permission to access this page`,
+                DT: ''
+            })
+        }
+
+
+    } else {
+        return res.status(401).json({
+            EC: -1,
+            EM: 'Unauthorized',
+            DT: ''
+        })
+    }
 }
 
 module.exports = {
-    createJWT, verifyJWT
+    createJWT, verifyJWT, checkUserJWT, checkUserPermission
 }
--- src/routes/api.js
+++ src/routes/api.js
@@ -2,31 +2,27 @@ import express from 'express';
 import apiController from '../controller/apiController';
 import userController from '../controller/userController';
 import groupController from '../controller/groupController';
+import { checkUserJWT, checkUserPermission } from '../middleware/JWTAction'
 
 const router = express.Router();
 
-const testMiddleware = (req, res, next) => {
-    console.log("test middleware");
-    next();
-}
 
 const initApiRoutes = (app) => {
 
     // rest api
     //rest api
 
-    router.get("/test-api", apiController.testApi);
     router.post("/register", apiController.handleRegister);
     router.post("/login", apiController.handleLogin);
 
 
     // GET-r POST-c PUT-u DELETE-d
-    router.get("/user/show", userController.showFunc);
-    router.post("/user/create", userController.createFunc);
+    router.get("/user/show", checkUserJWT, checkUserPermission, userController.showFunc);
+    router.post("/user/create", checkUserPermission, userController.createFunc);
     router.put("/user/update", userController.updateFunc);
     router.delete("/user/delete", userController.deleteFunc);
 
-    router.get("/user/show/page=?&limit=?", userController.showFunc);
+    // router.get("/user/show/page=?&limit=?", userController.showFunc);
     router.get("/group/show", groupController.showFunc);
 
 
