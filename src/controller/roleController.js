5f256cc76ba7d545c3b8a2be2faed6e0b148c432
giatha0 <thao8c89@gmail.com>
2023-06-19 22:48:50 +0700

    viet get role with paginate and get role by group

--- src/controller/roleController.js
+++ src/controller/roleController.js
@@ -1,19 +1,34 @@
 import roleApiService from '../service/roleApiService';
 
-const showRoleFunc = async () => {
+const showRoleFunc = async (req, res) => {
     try {
-        const data = await groupApiService.showRole();
-        return res.status(200).json({
-            EM: data.EM,
-            EC: data.EC,
-            data: data.DT,
-        });
+
+        if (req.query.page && req.query.limit) {
+            let page = req.query.page;
+            let limit = req.query.limit;
+            let data = await roleApiService.showRoleFuncWithPaginate(+page, +limit);
+            // console.log(data);
+            return res.status(200).json({
+                EM: data.EM,
+                EC: data.EC,
+                DT: data.DT,
+            })
+
+        } else {
+            let data = await roleApiService.showRoleFunc();
+
+            return res.status(200).json({
+                EM: data.EM,
+                EC: data.EC,
+                DT: data.DT,
+            })
+        }
     } catch (error) {
         console.log(error);
         return res.status(500).json({
             EM: 'Error from server',
             EC: '-1',
-            data: '',
+            DT: '',
         });
     }
 }
@@ -36,7 +51,43 @@ const createRoleFunc = async (req, res) => {
     }
 }
 
+const deleteRoleFunc = async (req, res) => {
+    try {
+        let data = await roleApiService.deleteRole(req.body.id);
+        return res.status(200).json({
+            EM: data.EM,
+            EC: data.EC,
+            data: data.DT,
+        });
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            EM: 'Error from server',
+            EC: '-1',
+            data: '',
+        });
+    }
+}
+
+const getRoleByGroupFunc = async (req, res) => {
+    try {
+        let id = req.params.groupId;
+        let data = await roleApiService.getRoleByGroup(id);
+        return res.status(200).json({
+            EM: data.EM,
+            EC: data.EC,
+            DT: data.DT,
+        });
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            EM: 'Error from server',
+            EC: '-1',
+            DT: '',
+        });
+    }
+}
 
 module.exports = {
-    showRoleFunc, createRoleFunc
+    showRoleFunc, createRoleFunc, deleteRoleFunc, getRoleByGroupFunc
 };
